6a82d3d87887a0fedcfeea93523b2c964f00a3f3
VidyaRajan7 <vidyar851@gmail.com>
2020-12-17 15:45:28 +0530

    Initial commit

---
+++ hala_app_sample/lib/common/widgets/widgets.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CommonWidgets extends StatelessWidget {
+
+  String title;
+  CommonWidgets({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 30, 15, 0),
+      width: (MediaQuery.of(context).size.width) - 20,
+      height: 60,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: 40,
+            height: 60,
+            child: FlatButton(
+              padding: EdgeInsets.all(10),
+              child: Image(image: new AssetImage('assets/icon_dots_vertical.png'),),
+              onPressed: ()=>{},
+            )
+          ),
+          
+          Center(
+            child: Container(
+              width: 300,
+              child: Text(this.title,
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold
+              ),textAlign: TextAlign.center,),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ hala_app_sample/lib/screens/login/view/login_ui.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:hala_app_sample/common/widgets/widgets.dart';
+
+class LoginUI extends StatefulWidget {
+  @override
+  _LoginUIState createState() => _LoginUIState();
+}
+
+class _LoginUIState extends State<LoginUI> {
+
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+  FocusNode emailFocus = new FocusNode();
+  FocusNode passwordFocus = new FocusNode();
+  bool emailError = false;
+  bool passwordError = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: (Stack(
+       children: <Widget>[
+         new Container(
+           decoration: new BoxDecoration(
+             image: new DecorationImage(image: new AssetImage('assets/background.png'), fit: BoxFit.cover)
+           ),
+         ),
+         new Container(
+           child: CommonWidgets(title: 'Login',),
+         ),
+         Center(
+           child: ListView(
+             shrinkWrap: true,
+             children: getLoginWidget(),
+           ),
+         )
+       ],
+     )),
+    );
+  }
+
+  List<Widget> getLoginWidget() {
+    List<Widget> loginWidget = new List();
+    Container co1 = new Container(
+      decoration: new BoxDecoration(
+        image: new DecorationImage(image: new AssetImage('assets/background.png'), fit: BoxFit.cover)
+      ),
+    );
+    loginWidget.add(co1);
+
+    loginWidget.add(
+      DecoratedBox(
+        decoration: new BoxDecoration(
+            image: new DecorationImage(image: new AssetImage('assets/background.png'), fit: BoxFit.cover)
+        ),
+      )
+    );
+    loginWidget.add(getLabelWidget('User Name'));
+    loginWidget.add(getTextFieldWidget('assets/icon_user.png', true));
+    loginWidget.add(getLabelWidget('Password'));
+    loginWidget.add(getTextFieldWidget('assets/icon_password.png', false));
+    Column c1 = new Column(
+      children: <Widget>[
+        Container(
+          width: (MediaQuery.of(context).size.width) - 30,
+          height: 50,
+          margin: EdgeInsets.fromLTRB(0, 40, 0, 10),
+          child: RaisedButton(
+            child: Text('Login',
+            style: TextStyle(color: Colors.white, fontSize: 21),
+            ),
+            color: Colors.blue,
+            onPressed: () => {},
+
+          ),
+        ),
+      ],
+    );
+    loginWidget.add(c1);
+    return loginWidget;
+  }
+
+  Widget getEmailWidget() {
+    return Center(
+      child: SizedBox(
+        child: Container(
+          color: Colors.white,
+          margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+          child: Container(
+            color: Colors.white,
+            child: TextFormField(
+              keyboardType:TextInputType.emailAddress,
+              controller: emailController,
+              focusNode: emailFocus,
+              onChanged: this.validate,
+              style: new TextStyle(color: Colors.grey),
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: false,
+                errorText: (emailError ? 'Invalid Email' : null),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getPasswordWidget() {
+    return Center(
+      child: SizedBox(
+        child: Container(
+          color: Colors.white,
+          margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+          child: Container(
+            color: Colors.white,
+            child: TextFormField(
+              keyboardType:TextInputType.visiblePassword,
+              controller: passwordController,
+              focusNode: passwordFocus,
+              onChanged: this.validate,
+              style: new TextStyle(color: Colors.grey),
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: false,
+                errorText: (passwordError ? 'Please Enter password' : null),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getLabelWidget(String title) {
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 15, 0, 10),
+          width: (MediaQuery.of(context).size.width) - 30,
+          child: Text(title,
+          style: new TextStyle(color: Colors.grey),),
+        )
+      ],
+    );
+  }
+
+  Widget getTextFieldWidget(String imageName, bool isEmail) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          width: (MediaQuery.of(context).size.width - 30),
+          height: 50,
+          child: Container(
+            color: Colors.white,
+            child: new Row(
+              children: <Widget>[
+                SizedBox(
+                  width: (MediaQuery.of(context).size.width) - 80,
+                  height: 60,
+                  child: new Container(
+                    margin: EdgeInsets.fromLTRB(15.0, 0.0, 0.0, 0.0),
+                    color: Colors.white,
+                    height: 50.0,
+                    child: isEmail ? getEmailWidget() : getPasswordWidget(),
+                  ),
+                ),
+                SizedBox(
+                  width: 35,
+                  height: 35,
+                  child: Container(
+                    color: Colors.white,
+                    child: SizedBox(
+                      child: Image.asset(imageName),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  void validate(String value) {
+    setState(() {
+      if (isEmailValid(emailController.value.text.toString())) {
+        emailError = false;
+      } else {
+        emailError = true;
+      }
+      if (passwordController.value.text.toString().length <= 0) {
+        passwordError = true;
+      } else {
+        passwordError = false;
+      }
+    });
+  }
+
+  bool isEmailValid(String emailValue) {
+    String emailExpression = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regExp = new RegExp(emailExpression);
+    return regExp.hasMatch(emailValue);
+  }
+}
